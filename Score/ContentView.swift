26ea4b2424fcf067c27aa407a3be1577886457b6
Modzie <modzielol@gmail.com>
2024-09-01 19:20:01 +0200

    Init: new project

---
+++ Score/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Score
+//
+//  Created by Thomas Meyer on 01/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Score/ScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreApp.swift
+//  Score
+//
+//  Created by Thomas Meyer on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
