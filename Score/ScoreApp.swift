96f92fc4e67ac36d084c4f6e0a5958fc0a7891f6
Modzie <modzielol@gmail.com>
2024-11-08 11:15:27 +0100

    Add: ersatz for admob

--- Score/ScoreApp.swift
+++ Score/ScoreApp.swift
@@ -5,10 +5,17 @@
 //  Created by Thomas Meyer on 01/09/2024.
 //
 
+
+import GoogleMobileAds
 import SwiftUI
+
 
 @main
 struct ScoreApp: App {
+    init() {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
