fcc7612703d126b689452491115685eb6fb908f3
Modzie <modzielol@gmail.com>
2024-11-13 14:54:35 +0100

    Add: class and their method to load a full screen interstitial ad

--- Score/Include/InterstitialAd.swift
+++ Score/Include/InterstitialAd.swift
@@ -9,3 +9,28 @@ import GoogleMobileAds
 import SwiftUI
 
 
+class InterstitialAd: NSObject, GADFullScreenContentDelegate, Observable {
+    private var interstitial: GADInterstitialAd?
+    @Published var adLoaded = false
+    
+    func loadInterstitial() {
+        let request = GADRequest()
+        GADInterstitialAd.load(withAdUnitID: "ca-app-pub-7069513903308677~7960370828", request: request) { ad, error in
+            if let error = error {
+                print("Error while loading: \(error.localizedDescription)")
+                return
+            }
+            self.interstitial = ad
+            self.interstitial?.fullScreenContentDelegate = self
+            self.adLoaded = true
+        }
+    }
+    
+    func showAd(from rootViewController: UIViewController) {
+        if let ad = self.interstitial {
+            ad.present(fromRootViewController: rootViewController)
+        } else {
+            print("Ad is not ready yet")
+        }
+    }
+}
