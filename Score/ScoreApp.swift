50b6aaa4b08769e5a3f362c29d1a50b6e0fc1035
Modzie <modzielol@gmail.com>
2024-11-28 16:57:58 +0100

    Refactor: admob

--- Score/ScoreApp.swift
+++ Score/ScoreApp.swift
@@ -6,15 +6,10 @@
 //
 
 import SwiftUI
-import GoogleMobileAds
 
 
 @main
 struct ScoreApp: App {
-    init() {
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
