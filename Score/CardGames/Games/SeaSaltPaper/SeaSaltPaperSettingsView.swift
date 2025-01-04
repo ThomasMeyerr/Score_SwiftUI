e986d0f0b78bdf5e1cc422bd0221829475e54fdf
Modzie <modzielol@gmail.com>
2025-01-04 18:42:47 +0100

    Add: SeaSaltPaper rule in data

--- Score/CardGames/Games/SeaSaltPaper/SeaSaltPaperSettingsView.swift
+++ Score/CardGames/Games/SeaSaltPaper/SeaSaltPaperSettingsView.swift
@@ -9,12 +9,122 @@ import SwiftUI
 
 
 struct SeaSaltPaperSettingsView: View {
+    @Environment(Data.self) var data
+    
+    @State private var numberOfPlayer = 2
+    @State private var maxScore: Double = 500
+    @State private var names: [String] = Array(repeating: "", count: 2)
+    @State private var isShowingAlert = false
+    @State private var isPartyOngoing = UserDefaults.standard.bool(forKey: "partySeaSaltPaperOngoing")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Form {
+                    Section(getText(forKey: "players", forLanguage: data.languages)) {
+                        loadPlayersList()
+                    }
+                    
+                    Section(getText(forKey: "maxScore", forLanguage: data.languages)) {
+                        HStack {
+                            Text(String(Int(maxScore)))
+                            Slider(value: $maxScore, in: 400...600, step: 10)
+                        }
+                    }
+                    
+                    Section(getText(forKey: "rules", forLanguage: data.languages)) {
+                        RulesText(text: getRules(forKey: "seaSaltPaper", forLanguage: data.languages), language: data.languages)
+                    }
+                }
+                
+                loadButtons()
+            }
+            .onAppear {
+                resetData()
+            }
+            .alert(isPresented: $isShowingAlert) {
+                Alert(
+                    title: Text(getText(forKey: "alertTitle", forLanguage: data.languages)),
+                    message: Text(getText(forKey: "alertMessage", forLanguage: data.languages)),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
+        }
+        .navigationTitle(getText(forKey: "settings", forLanguage: data.languages) + "Sea Salt & Paper")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    func loadPlayersList() -> some View {
+        Section {
+            Picker(getText(forKey: "numberOfPlayers", forLanguage: data.languages), selection: $numberOfPlayer) {
+                ForEach(2..<11, id: \.self) {
+                    Text(String($0))
+                }
+            }
+            .onChange(of: numberOfPlayer) { oldValue, newValue in
+                if names.count < newValue {
+                    names.append(contentsOf: Array(repeating: "", count: newValue - names.count))
+                } else if names.count > newValue {
+                    names.removeLast(names.count - newValue)
+                }
+            }
+            
+            ForEach(0..<numberOfPlayer, id: \.self) { index in
+                TextField(getText(forKey: "pseudo", forLanguage: data.languages), text: Binding(
+                    get: { names.indices.contains(index) ? names[index] : "" },
+                    set: { newValue in
+                        if names.indices.contains(index) {
+                            names[index] = newValue
+                        }
+                    }
+                ))
+            }
+        }
+    }
+    
+    func loadButtons() -> some View {
+        HStack {
+            if isPartyOngoing {
+                Spacer()
+                NavigationLink(getText(forKey: "continue", forLanguage: data.languages)) {
+                    UnoView(numberOfPlayer: numberOfPlayer, maxScore: maxScore, names: names, isNewGame: false)
+                }
+                .padding()
+                .foregroundStyle(.white)
+                .background(.green)
+                .cornerRadius(10)
+                .frame(height: 30)
+            }
+            Spacer()
+            NavigationLink(getText(forKey: "launch", forLanguage: data.languages)) {
+                UnoView(numberOfPlayer: numberOfPlayer, maxScore: maxScore, names: names, isNewGame: true)
+            }
+            .padding()
+            .foregroundStyle(.white)
+            .background(names.contains(where: { $0.isEmpty }) || names.count != Set(names).count ? .gray : .blue)
+            .cornerRadius(10)
+            .frame(height: 30)
+            .disabled(names.contains(where: { $0.isEmpty }) || names.count != Set(names).count)
+            .onTapGesture {
+                if (names.count != Set(names).count) && !names.contains(where: { $0.isEmpty }) {
+                    isShowingAlert = true
+                }
+            }
+            Spacer()
+        }
+        .padding()
+    }
+    
+    func resetData() {
+        isPartyOngoing = UserDefaults.standard.bool(forKey: "partySeaSaltPaperOngoing")
+        numberOfPlayer = 2
+        maxScore = 500
+        names = Array(repeating: "", count: 2)
     }
 }
 
 
 #Preview {
     SeaSaltPaperSettingsView()
+        .environment(Data())
 }
