13dc3e04cf590818753994f10094432cac72e1bd
Modzie <modzielol@gmail.com>
2025-01-20 12:03:05 +0100

    Add: new game: take 6

---
+++ Score/CardGames/Games/6 qui prend !/Take6SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  Take6SettingsView.swift
+//  Score
+//
+//  Created by Thomas Meyer on 20/01/2025.
+//
+
+import SwiftUI
+
+struct Take6SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Take6SettingsView()
+}
